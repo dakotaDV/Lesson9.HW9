b1e9b3989e3c77bf3587a63c8522f8e9301d3c57
Prudnikova <dakoto4ka@yandex.ru>
2022-09-03 02:11:59 +0300

    дз сделано

---
+++ src/HW9/Main.java
@@ -0,0 +1,28 @@
+package HW9;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args){
+        var author1 =  new Author("Лев", "Толстой");
+        var author2 = new Author("Федор", "Достоевский");
+        var author3 = new Author("Михаил", "Булгаков");
+
+        var book1 = new Book("Война и мир", author1, 1867);
+        var book2 = new Book("Белые ночи", author2, 1848);
+        var book3 = new Book("Мастер и Маргарита", author3, 1967);
+
+
+        book1.setYearPublication(LocalDate.now().getYear());
+
+        System.out.println(author1);
+        System.out.println(book1);
+
+
+
+    }
+
+
+
+    }
+
